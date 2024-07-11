a17e00b7218de99cccf902e9128735026f7cc856
sufyanserdah <67437098+sufyanserdah@users.noreply.github.com>
2024-07-10 23:16:12 -0700

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+arg=$1
+echo $arg
+source ./$arg.sh
+sam deploy -t pipeline.yml \
+    --stack-name ${ResourcePrefix}-${Environment}-Backend \
+    --region=${REGION} \
+    --no-fail-on-empty-changeset \
+    --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM \
+    --parameter-overrides \
+        Environment=${Environment} \
+        BackendRepoName=${BackendRepoName} \
+        Region=${REGION} \
+        MainGitBranch=${MainGitBranch} \
+        StackName=${ResourcePrefix}-${Environment}-resources \
+        PipelineArtifactBucket=${ResourcePrefix}-${Environment}-backend-deployments \
+        Tags="${Tags}" \
+        IsS3BucketExists=${IsS3BucketExists} \
+        ResourcePrefix=${ResourcePrefix} \
+        SourceCodeProvider=CodeCommit 
